30d4794e1e34a6d47543679ec813268234cc5ff5
Manuel Freitas <55348435+manuelfreitasc@users.noreply.github.com>
2022-08-08 09:33:44 +0100

    first commit

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+
+    async login({ request, response, auth }){
+        const { email, password } = request.only(['email','password'])
+
+        const token = await auth.attempt(email,password)
+
+        return response.json(token)
+    }
+
+    async register({ request, response}){
+        const { name, email, password } = request.only(['name','email','password'])
+        console.log(name,email,password)
+        await User.create({name,email,password})
+
+        return response.send({ message: "User has been created" })
+    }
+
+    async show({ response, params }){
+        const user = request.find(params.id)
+
+        const res = {
+            name: user.name,
+            email: user.email
+        }
+
+        return response.json(res)
+    }
+}
+
+module.exports = UserController
